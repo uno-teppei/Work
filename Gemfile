1141d814158fdaef9531ee0c0699557fea9edf07
unoteppei <pasuta46@icloud.com>
2023-03-24 16:45:28 +0900

    メール認証更新

--- Gemfile
+++ Gemfile
@@ -72,3 +72,6 @@ gem 'devise'
 
 #日本語化
 gem 'rails-i18n'
+
+#環境変数
+gem 'dotenv-rails'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,14 +62,14 @@ Rails.application.configure do
 
   #デバイスメール設定
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    enable_starttls_auto: true,
-    authentication: "plain",
-    user_name: Rails.application.credentials.gmail[:user_name],
-    password: Rails.application.credentials.gmail[:password]
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => ENV['GMAIL_ADDRESS'],
+    :password => ENV['GMAIL_PASSWORD'],
+    :authentication => 'login'
   }
-  
 end
