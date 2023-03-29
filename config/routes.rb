fa13f6d770a50a4a81a7e82579e518e60009d440
unoteppei <pasuta46@icloud.com>
2023-03-29 15:24:41 +0900

    パスワードなしで更新できるように

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
 
   root 'pages#home'
   
-  devise_for :users
+  devise_for :users,
+              path: '', 
+              path_names: {sign_up: 'register', sign_in: 'login', edit: 'profile', sign_out: 'logout'},
+              controllers: {registrations: 'registrations'}
+
   get 'pages/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
