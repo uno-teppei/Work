cd952896674958aa570f6b578052a0d5896d694f
unoteppei <pasuta46@icloud.com>
2023-04-15 21:00:51 +0900

    プロフィール詳細 id route設定

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   get 'pages/home'
   get '/character', to: 'users#character'
+  get '/users/:id', to: 'users#show', as: 'user'
 
   post '/users/edit', to: 'users#update'
 end
