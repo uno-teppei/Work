3b012d857a4617295e8e3e9966361cde35d5466b
unoteppei <pasuta46@icloud.com>
2023-04-22 21:01:49 +0900

    バリデーション追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  validates :airport_name, presence: true
+  validates :prefecture, presence: true
+  validates :impression, presence: true, length: {minimum: 10}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
 
+  has_many :posts
   has_one_attached :avatar
   
   # Include default devise modules. Others available are:
