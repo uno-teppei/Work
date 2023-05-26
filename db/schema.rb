155653df8714a02346fd8040ba6c11213e4c3bbc
unoteppei <pasuta46@icloud.com>
2023-05-26 20:16:48 +0900

    画像フォーム作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_26_112711) do
+ActiveRecord::Schema.define(version: 2023_05_26_110715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2023_04_26_112711) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.json "images"
+    t.binary "image"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
