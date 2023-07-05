dabc6fe0007581260af9aa5f6927ffd778a68ed1
unoteppei <pasuta46@icloud.com>
2023-07-05 14:30:02 +0900

    ルート メソッド設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,23 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[google_oauth2]
+
   validates :full_name, presence: true, length: {maximum: 15}
+
+  def self.from_omniauth(auth)
+    user = User.where(email: auth.info.email).first
+    if user
+      return user
+    else
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0, 20]
+        user.full_name = auth.info.name   # ユーザーモデルに名前があると仮定
+        user.image = auth.info.image # ユーザーモデルに画像があると仮定
+        user.uid = auth.uid
+        user.provider = auth.provider
+      end
+    end
+  end
 end
