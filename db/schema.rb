4d90ee96cfa2ee68ac8558a25b6712e0bdbd2e44
unoteppei <pasuta46@icloud.com>
2023-07-05 12:02:26 +0900

    nilが存在していたのでreset

--- db/schema.rb
+++ db/schema.rb
@@ -57,13 +57,9 @@ ActiveRecord::Schema.define(version: 2023_07_04_113402) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "full_name"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
     t.text "about"
     t.boolean "status", default: false
     t.string "image"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
