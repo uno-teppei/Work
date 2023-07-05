1a0faf8db598a3f3e04875f7817e70738cdf3a7d
unoteppei <pasuta46@icloud.com>
2023-07-05 19:00:20 +0900

    成功時の処理で進むため変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,7 +15,7 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
-    if @post.save!
+    if @post.save
       flash[:notice] = "投稿を作成しました"
       redirect_to posts_path
     else
