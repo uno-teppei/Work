d8b57e0c73035eb30a90884cba61c0766d1204e5
unoteppei <pasuta46@icloud.com>
2023-07-19 17:40:03 +0900

    high_voltageインストールのため変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
 
-  root 'pages#home'
+  root 'tops#home'
   
   devise_for :users,
               path: '', 
               path_names: {sign_up: 'register', sign_in: 'login', edit: 'profile', sign_out: 'logout'},
               controllers: {omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations'}
 
-  get 'pages/home'
+  get 'tops/home'
   get '/character', to: 'users#character'
   get '/users/:id', to: 'users#show', as: 'user'
   get 'posts/search', to: 'posts#search'
