9dd43f3e00a37ba2799d4062341da93f0c93b7a5
unoteppei <pasuta46@icloud.com>
2023-07-21 20:25:19 +0900

    地図作成準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,19 @@
 Rails.application.routes.draw do
 
-  root 'tops#home'
+  root to: 'maps#index'
   
   devise_for :users,
               path: '', 
               path_names: {sign_up: 'register', sign_in: 'login', edit: 'profile', sign_out: 'logout'},
               controllers: {omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations'}
 
-  get 'tops/home'
+  get 'maps/index'
   get '/character', to: 'users#character'
   get '/users/:id', to: 'users#show', as: 'user'
   get 'posts/search', to: 'posts#search'
 
   post '/users/edit', to: 'users#update'
-
+  
+  resources :maps, only: [:index]
   resources :posts
 end
