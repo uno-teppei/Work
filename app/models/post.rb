81ca03bbde3292526cfc0f4d811410045da91b06
unoteppei <pasuta46@icloud.com>
2023-07-25 19:48:39 +0900

    不要になり排除

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,6 @@ class Post < ApplicationRecord
   has_many_attached :image
 
   validates :airport_name, presence: true
-  validates :prefecture, presence: true
-  validates :impression, presence: true, length: {minimum: 10}
+  validates :impression, presence: true, length: {minimum: 5}
   validates :image, presence: true
 end
