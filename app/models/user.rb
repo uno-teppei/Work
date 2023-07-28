9766c0088ea0aa92e2437355d251724bd05de7c9
unoteppei <pasuta46@icloud.com>
2023-07-28 10:18:34 +0900

    tab移動

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[google_oauth2]
+          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[google_oauth2]
 
   validates :full_name, presence: true, length: {maximum: 15}
 
