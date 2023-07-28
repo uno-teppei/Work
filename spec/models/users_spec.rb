6465b2003906b3d3c68f3daab83d2690ecb9d221
unoteppei <pasuta46@icloud.com>
2023-07-28 11:12:32 +0900

    登録できない時のテスト追加

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -6,5 +6,23 @@ RSpec.describe User, type: :model do
       user = build(:user)
       expect(user).to be_valid 
     end
+    it "full_nameを入力していないとき登録できないこと" do
+      expect(subject).not_to be_valid
+      expect(subject.errors).not_to be_empty
+    end
+    it "emailを入力していないとき登録できないこと" do
+      expect(subject).not_to be_valid
+      expect(subject.errors).not_to be_empty
+    end
+    it "passwordを入力していないとき登録できないこと" do
+      expect(subject).not_to be_valid
+      expect(subject.errors).not_to be_empty
+    end
+    it "重複したメールアドレスが存在するとき登録できないこと" do
+      user = create(:user)
+      subject.email = user.email
+      expect(subject).not_to be_valid
+      expect(subject.errors[:email]).to include("はすでに存在します")
+    end
   end
 end
